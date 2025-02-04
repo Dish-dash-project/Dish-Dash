9f75b058a81c12ec62fd0c179e5c29b9c4eb4de8
achref-fraj <101942901+achref-fraj@users.noreply.github.com>
2025-02-04 10:33:28 -1100

    accepted

--- backend/server/server.js
+++ backend/server/server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.use(cors())
 // server configuration
 // make the server listen to requests
+
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });

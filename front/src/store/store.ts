197aaf42b7b0ea308a34112c5e6d55848cb81100
achref-fraj <101942901+achref-fraj@users.noreply.github.com>
2025-02-06 06:21:19 -1100

    accepted

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,15 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slice/authslice';
+import filterProductByQuery from './slice/filterProductByQuery.ts';
+import orderReducer from './slice/orderSlice.ts';
+import categoryReducer from './slice/categorySlice.ts';
 import chatReducer from './slice/chatSlice';
-import customerReducer from './slice/customerSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    chat: chatReducer,
-    customers: customerReducer,
+    filterProductByQuery:filterProductByQuery,
+    order: orderReducer ,
+    category:categoryReducer,
+    chat: chatReducer
   },
 });
 
+
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch; 
+export type AppDispatch = typeof store.dispatch;
